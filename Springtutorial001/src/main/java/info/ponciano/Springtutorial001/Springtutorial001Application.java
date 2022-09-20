d48e2cfe02ad9570b97e4764fa17ef8cdc5f4e8c
Claire Ponciano <prudh.claire@gmail.com>
2022-09-20 15:29:50 +0200

    Create Springtutorial001Application.java

---
+++ Springtutorial001/src/main/java/info/ponciano/Springtutorial001/Springtutorial001Application.java
@@ -0,0 +1,13 @@
+package info.ponciano.Springtutorial001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springtutorial001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springtutorial001Application.class, args);
+	}
+
+}
