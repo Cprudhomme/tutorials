feca031e9b60cf31c654e209ff01f843c094d59b
Claire Ponciano <prudh.claire@gmail.com>
2022-09-20 15:29:46 +0200

    Create Springtutorial001ApplicationTests.java

---
+++ Springtutorial001/src/test/java/info/ponciano/Springtutorial001/Springtutorial001ApplicationTests.java
@@ -0,0 +1,13 @@
+package info.ponciano.Springtutorial001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springtutorial001ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
